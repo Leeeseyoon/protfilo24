1ae3d027cfb3fa3de8225597a664c961d80c38ce
Leeeseyoon <lsy200400@naver.com>
2024-11-10 22:42:59 +0900

    밀고

--- js/textover.js
+++ js/textover.js
@@ -14,7 +14,7 @@ let particleArray = [];
 let adjustX = 90;
 let adjustY = 85;
 const particleAttributes = {
-	size : 2,
+	size: 2,
 	color: '#7300006f',
 	kerning: 4.2,
 	returnLag: 10,
@@ -26,36 +26,43 @@ const mouse = {
 	radius: 100
 }
 
-/* 폰트 스타일 변경 */
-ctx.font = 'italic 87px  SF_HambakSnow';
-ctx.fillText('Tomato', 30, 62 );
-ctx.fillText('Puree', 90, 130 );
-const textCoordinates = ctx.getImageData(15, 0, 350, 4000)
+let textCoordinates; // 텍스트 좌표를 저장할 변수
 
+/* 폰트 로드 함수 */
+async function loadFont() {
+	const font = new FontFace('SF_HambakSnow', 'url(https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2106@1.1/SF_HambakSnow.woff)'); // 폰트 파일 경로를 올바르게 설정하세요
+	await font.load();
+	document.fonts.add(font);
+	drawText(); // 폰트가 로드된 후 텍스트를 그리는 함수 호출
+}
 
-window.addEventListener('mousemove',
-    function (event) {
-        mouse.x = event.x;
-        mouse.y = event.y;
-    }
-)
-window.addEventListener("touchmove", 
-    function (event) {
-        let touch = event.touches[0];
-        mouse.x = touch.clientX;
-        mouse.y = touch.clientY;
-    }
-)
+/* 텍스트 그리기 함수 */
+function drawText() {
+	ctx.font = 'italic 87px SF_HambakSnow';
+	ctx.fillText('Tomato', 30, 62);
+	ctx.fillText('Puree', 90, 130);
+	textCoordinates = ctx.getImageData(15, 0, 350, 4000); // 텍스트 좌표를 가져옴
+	init(); // 파티클 초기화
+}
 
+window.addEventListener('mousemove', function (event) {
+	mouse.x = event.x;
+	mouse.y = event.y;
+});
+window.addEventListener("touchmove", function (event) {
+	let touch = event.touches[0];
+	mouse.x = touch.clientX;
+	mouse.y = touch.clientY;
+});
 window.addEventListener('resize', () => {
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-    canvas.style.bottom = '0';
-    canvas.style.right = '0';
+	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
+	canvas.style.bottom = '0';
+	canvas.style.right = '0';
 });
 
 class Particle {
-	constructor(x,y) {
+	constructor(x, y) {
 		this.x = x;
 		this.y = y;
 		this.size = particleAttributes.size;
@@ -66,34 +73,34 @@ class Particle {
 		this.size = 0;
 	}
 	draw() {
-		ctx.fillStyle = particleAttributes.color
+		ctx.fillStyle = particleAttributes.color;
 		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+		ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
 		ctx.closePath();
 		ctx.fill();
 	}
 	update() {
 		let dx = mouse.x - this.x;
-		let dy = mouse.y - this.y; 
-		let distance =  Math.sqrt(dx * dx + dy * dy);
+		let dy = mouse.y - this.y;
+		let distance = Math.sqrt(dx * dx + dy * dy);
 		let forceDirectionX = dx / distance;
 		let forceDirectionY = dy / distance;
-		let maxDistance = mouse.radius
-		let force = (maxDistance - distance) / maxDistance
+		let maxDistance = mouse.radius;
+		let force = (maxDistance - distance) / maxDistance;
 		let directionX = forceDirectionX * force * this.density;
 		let directionY = forceDirectionY * force * this.density;
 
 		if (distance < mouse.radius) {
-			this.x -= directionX; 
+			this.x -= directionX;
 			this.y -= directionY;
 		} else {
 			if (this.x != this.baseX) {
 				let dx = this.x - this.baseX;
-				this.x -= dx / particleAttributes.returnLag
+				this.x -= dx / particleAttributes.returnLag;
 			}
 			if (this.y != this.baseY) {
 				let dy = this.y - this.baseY;
-				this.y -= dy / particleAttributes.returnLag
+				this.y -= dy / particleAttributes.returnLag;
 			}
 		}
 
@@ -105,19 +112,18 @@ class Particle {
 
 function init() {
 	particleArray = [];
-	for(let y = 0; y < textCoordinates.height; y++) {
-		for(let x = 0; x < textCoordinates.width; x++) {
-			if (textCoordinates.data[(y * 4 * textCoordinates.width) +(x*4) + 3] > 128) {
+	for (let y = 0; y < textCoordinates.height; y++) {
+		for (let x = 0; x < textCoordinates.width; x++) {
+			if (textCoordinates.data[(y * 4 * textCoordinates.width) + (x * 4) + 3] > 128) {
 				let postionX = x + adjustX;
-				let positionY = y  + adjustY;
-				particleArray.push(new Particle(postionX * particleAttributes.kerning, positionY * particleAttributes.kerning))
-				
+				let positionY = y + adjustY;
+				particleArray.push(new Particle(postionX * particleAttributes.kerning, positionY * particleAttributes.kerning));
 			}
 		}
 	}
 }
 
-init();
+loadFont(); // 폰트 로드 시작
 
 function animate() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -125,7 +131,7 @@ function animate() {
 		particleArray[i].draw();
 		particleArray[i].update();
 	}
-	requestAnimationFrame(animate); 
+	requestAnimationFrame(animate);
 }
 
 animate();
